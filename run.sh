fb003d49d6981322097dac3ad7db948c0934e711
Tom Jankauski <tomjankauski@gmail.com>
2025-07-10 21:53:19 -0700

    use a temp folder for the output

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ SPEECH_FILE=$(mktemp)
 rec -v 2 -d -c 1 -t wav $SPEECH_FILE silence 1 1 0.1% 1 0:02 0.1%
 
 # Transcribe the audio
-whisper $SPEECH_FILE --model tiny.en --device cuda -o out --output_format txt
+OUTPUT_DIR=$(mktemp -d)
+whisper $SPEECH_FILE --model tiny.en --device cuda -o $OUTPUT_DIR --output_format txt
 
 file_name=$(basename ${SPEECH_FILE%.*})
-cat "out/$file_name.txt"
+cat "$OUTPUT_DIR/$file_name.txt"
 rm $SPEECH_FILE
