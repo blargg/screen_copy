40d20fa48d57ce566439e9f805ad1317f92a17b5
Tom Jankauski <tomjankauski@gmail.com>
2025-07-14 21:59:33 -0700

    add option to type the output

--- pkgs/whisper-transcribe.nix
+++ pkgs/whisper-transcribe.nix
@@ -1,7 +1,7 @@
-{ config, writeShellApplication, openai-whisper, sox }:
+{ config, writeShellApplication, openai-whisper, sox, xdotool }:
 writeShellApplication {
   name = "whisper-transcribe";
-  runtimeInputs = [ openai-whisper sox ];
+  runtimeInputs = [ openai-whisper sox xdotool ];
 
   text =
     ''
@@ -14,9 +14,9 @@ writeShellApplication {
         echo "Usage: whisper-transcribe [-d <device>] [-m <model>]
 
         Example:
-        whisper-transcribe -d cuda -m tiny.en
+        whisper-transcribe -d cuda -m tiny.en -t
 
-        This will run the model on the GPU with tiny.en, a small model that is quick to load, but only supports english.
+        This will run the model on the GPU using the  tiny.en model, and types the text out to the current cursor location.
 
         -d: The device to run on, (cpu, cuda)
         -m: The model to use, see https://github.com/openai/whisper?tab=readme-ov-file#available-models-and-languages
@@ -24,10 +24,11 @@ writeShellApplication {
       }
 
 
-      while getopts 'd:m:h' flag; do
+      while getopts 'd:m:ht' flag; do
         case "''${flag}" in
           d) DEVICE="''${OPTARG}" ;;
           m) MODEL="''${OPTARG}" ;;
+          t) TYPE_OUTPUT=true ;;
           h) print_usage
           exit 1 ;;
           *) print_usage
@@ -44,7 +45,12 @@ writeShellApplication {
       whisper "$SPEECH_FILE" ''${MODEL:+--model "$MODEL"} ''${DEVICE:+--device "$DEVICE"} -o "$OUTPUT_DIR" --output_format txt > /dev/null
 
       file_name=$(basename "''${SPEECH_FILE%.*}")
-      cat "$OUTPUT_DIR/$file_name.txt"
+      FILE_PATH="$OUTPUT_DIR/$file_name.txt"
+      if [ "$TYPE_OUTPUT" = true ]; then
+        xdotool type "$(cat "$FILE_PATH")"
+      else
+        cat "$FILE_PATH"
+      fi
       rm "$SPEECH_FILE"
       rm -r "$OUTPUT_DIR"
     '';
